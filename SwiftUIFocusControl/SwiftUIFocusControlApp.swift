f48662c15ca0984b3e80c8930b2e3eb5295fcb10
John Siracusa <siracusa@gmail.com>
2025-01-04 13:24:04 -0500

    Initial Commit

---
+++ SwiftUIFocusControl/SwiftUIFocusControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFocusControlApp.swift
+//  SwiftUIFocusControl
+//
+//  Created by John Siracusa on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFocusControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
